50077ccfbe56be13738252b1757299e1160fdb6b
Thomas McNamara <thomasmcnamara@vt.edu>
2024-05-20 21:26:49 -0400

    very basic login page implemented

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+
+import { AuthService } from '../auth.service';
+
 @Component({
   selector: 'app-login-page',
   standalone: true,
@@ -8,5 +11,19 @@ import { Component } from '@angular/core';
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+  auth: AuthService;
 
+
+  constructor(auth: AuthService) {
+    this.auth = auth;
+  }
+
+  handleSubmit(username: string, password: string) {
+    this.auth.login(username, password).subscribe(
+      data => {
+        // here is where we will handle different cases
+        console.log(data);
+      }
+    );
+  }
 }
