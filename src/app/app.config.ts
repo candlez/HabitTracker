00e04dfc43c24e0fc489af64feb604d778f5b87c
Thomas McNamara <thomasmcnamara@vt.edu>
2024-08-31 13:37:43 -0400

    feat: habit form header complete

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,9 +7,11 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
     provideRouter(routes), 
     provideClientHydration(),
     importProvidersFrom(HttpClientModule),
--- src/app/components/habit-form/habit-form.component.ts
+++ src/app/components/habit-form/habit-form.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgIf } from '@angular/common';
+
+import { MatDatepickerInputEvent, MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {provideNativeDateAdapter} from '@angular/material/core';
+
+import { HabitService, Habit } from '../../services/habit.service';
+import { FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-habit-form',
   standalone: true,
-  imports: [],
+  imports: [MatDatepickerModule, MatButtonToggleModule, ReactiveFormsModule, FormsModule, MatInputModule,
+    MatFormFieldModule, NgIf
+  ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './habit-form.component.html',
   styleUrl: './habit-form.component.css'
 })
-export class HabitFormComponent {
+export class HabitFormComponent implements OnInit {
+  habits!: Habit[];
+  habitsLoaded: boolean = false;
 
+  toggleController: FormControl<string | null> = new FormControl<string | null>("today");
+
+  today!: string;
+  dateController: FormControl<Date | null> = new FormControl<Date | null>(new Date());
+
+  constructor(public habitService: HabitService) {
+
+  }
+
+  ngOnInit(): void {
+    this.habitService.getHabits().subscribe({
+      next: (habits: Habit[]) => {
+        this.habits = [];
+        for (let i: number = 0; i < habits.length; i++) {
+          if (habits[i].enabled) {
+            this.habits.push(habits[i]);
+          }
+        }
+        if (this.habits.length === 0) {
+          // same as 404
+        }
+        this.habitsLoaded = true;
+      },
+      error: (error: HttpErrorResponse) => {
+        if (error.status === 404) {
+          // see above
+        } else {
+          // generic error
+        }
+      }
+    });
+  }
+
+  handleDateSelect(event: MatDatepickerInputEvent<Date>): void {
+
+  }
 }
