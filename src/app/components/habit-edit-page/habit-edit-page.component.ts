51940fe85c811af8245f0b76a2579a62e89290b8
Thomas McNamara <thomasmcnamara@vt.edu>
2024-09-06 22:40:14 -0400

    feat: basic ui of habit editor complete

--- src/app/components/habit-edit-page/habit-edit-page.component.ts
+++ src/app/components/habit-edit-page/habit-edit-page.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgIf, NgFor, NgClass } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+
+import { HabitService, Habit } from '../../services/habit.service';
 
 @Component({
   selector: 'app-habit-edit-page',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor, NgClass, MatButtonModule, MatIcon],
   templateUrl: './habit-edit-page.component.html',
   styleUrl: './habit-edit-page.component.css'
 })
-export class HabitEditPageComponent {
+export class HabitEditPageComponent implements OnInit {
+  habits!: Habit[];
+  habitsLoaded: boolean = false;
 
+  errMsg: string | undefined = undefined;
+
+  constructor(public habitService: HabitService) {
+
+  }
+
+  ngOnInit(): void {
+      this.habitService.getHabits().subscribe({
+        next: (values: Habit[]) => {
+          this.habits = values;
+          this.habitsLoaded = true;
+        },
+        error: (error: HttpErrorResponse) => {
+          this.habitsLoaded = true;
+          if (error.status === 404) {
+
+          } else {
+
+          }
+        }
+      });
+  }
 }
